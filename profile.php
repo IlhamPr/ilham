6c8072836c57d85add4df4825281f88444d671f8
Ilham <33593988+IlhamPr@users.noreply.github.com>
2017-12-24 13:41:53 +0700

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Ilham's Web</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="container">
+  <?php include('header.php'); ?>
+  <?php include('menu.php'); ?>
+  <div class="content">
+    <h1>Tentang Saya</h1>
+    <p><img src="gue.jpg" width="450" height="300" /></p>
+    <p>Nama saya adalah Ilham.... Yahahah lengkapnya sih Ilham Pratama. Asal Banjarmasin, lulusan TKJ SMK Negeri 5 Banjarmasin. Terus ngelanjutin ke jurusan Sistem Informasi di Uniska Banjarmasin.</p>
+    <p>Kalau skripsi sih, saya inginnya mengambil Android. Karena ini zamannya Mobile Smartphone.</p>
+    <p><strong>Status? </strong>Hahaa, tentu saja status saya mahasiswa. Tapi sih, jika memiliki modal, saya ingin membuka kursus pembelajaran komputer-smartphone.</p>
+    <p>Impian? Impian sih ingin bisa membuat produk pencipta AR (Augmented Reality, terinspirasi dari anime Sword Art Online yang movie itu loh hihi :p).</p>
+  </div>
+  
+  <?php include('footer.php'); ?>
+  <!-- end .container --></div>
+</body>
+</html>
