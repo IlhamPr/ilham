68a98165fde4480efb5fdf0d2e17ca9ab356fd6d
Ilham <33593988+IlhamPr@users.noreply.github.com>
2017-12-24 13:41:13 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,10 @@
+ <div class="sidebar1">
+    <ul class="nav">
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="profile.php">Profile</a></li>
+      <li><a href="article.php">Article</a></li>
+      <li><a href="register.php">Register</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+    <p> Selamat datang di website pertama saya.</p>
+    <!-- end .sidebar1 --></div>
