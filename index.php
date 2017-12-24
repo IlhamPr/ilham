a0011f33a7d16da2d8c57a58db784c6192d10414
Ilham <33593988+IlhamPr@users.noreply.github.com>
2017-12-24 13:40:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="https://readfaced.files.wordpress.com/2014/04/i.jpg"/>
+<title>Ilham's Web</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="container">
+  <?php include('header.php'); ?>
+  <?php include('menu.php'); ?>
+  <div class="content">
+    <h1>Selamat Datang</h1>
+    <p>Selamat datang di website saya, silahkan memilih menu disamping kiri.</p>
+    <p><img src="83555f.jpg" width="300" /></p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <!-- end .content --></div>
+  
+  <?php include('footer.php'); ?>
+  <!-- end .container --></div>
+</body>
+</html>
