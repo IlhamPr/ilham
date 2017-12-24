5e3551deb1270d2a5e7ebdfa1b872f4b3796527a
Ilham <33593988+IlhamPr@users.noreply.github.com>
2017-12-24 13:39:04 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,3 @@
+<div class="header">
+<a href="#"><img src="edtfatal.png" alt="Insert Logo Here" name="Insert_logo" width="961" height="130" id="Insert_logo" style="background: #C6D580; display:block;" /></a> 
+    <!-- end .header --></div>
